6aa476a5bfa4fb0fa3103a24fa697f51db5ff49b
Lutfi Aldri <lutfialdripermana@gmail.com>
2024-11-14 16:11:32 +0700

    first commit

---
+++ cat_images_app/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:cat_images/presentation/notifier/cat_notifier.dart';
+import 'package:cat_images/presentation/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'locator.dart' as di;
+
+void main() {
+  di.init();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => di.locator<CatNotifier>(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage(),
+      ),
+    );
+  }
+}
